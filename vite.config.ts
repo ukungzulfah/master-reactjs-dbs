d8ff46f194e2a2865c357966f8b10ed7de7aeccd
ukung <ukung@ukungs-MacBook-Pro.local>
2025-10-28 17:22:24 +0700

    Update Repi Autocomplete TextField

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true, // atau host: '0.0.0.0'
+//     port: 5173, // port default Vite
+//   },
+// });
+
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: true, // atau host: '0.0.0.0'
+    port: 5173, // port default Vite
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/**/*',
+          dest: 'assets' // ini untuk /assets/
+        },
+        {
+          src: 'src/assets/**/*',
+          dest: 'src/assets' // ini untuk /src/assets/ fallback
+        }
+      ]
+    }),
+    react({
+      babel: {
+        plugins: [
+          ["@babel/plugin-proposal-decorators", { "legacy": true }],
+          ["@babel/plugin-proposal-class-properties", { "loose": true }]
+        ]
+      }
+    }),
+  ],
 })
